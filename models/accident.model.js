f7ddbbc05a00670c18ac8d8f9ec5b49a10d99f5c
Dominic <otienodominic@gmail.com>
2020-09-09 12:02:17 +0300

    optimise Backend

---
+++ models/accident.model.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+const schema = mongoose.Schema; 
+// Create a schema;
+const accidentSchema = new schema(
+  {
+    case_id: {
+      type: String,
+      unique: true
+    },
+    no_of_victims: {
+      type: String,
+      required: true,
+      unique: false
+    },  
+    device_type: {
+      type: String,
+      required: true,
+      unique:false
+    },
+    reported_time: {
+      type: Date,
+      required: true
+    },
+    location: {
+      submitted_location:{
+        type:String,
+        required:true
+      },
+      latitude:{
+        type: Number,
+        required:false
+      },
+      longitude:{
+        type:Number,
+        required:false
+      }
+    },
+    weather_type: {
+      type: String,
+      required: false
+    },
+    description:{
+      type:String,
+      required:false
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt'
+    }
+  }
+);
+
+
+accidentSchema.plugin(uniqueValidator);
+const accidentModel = mongoose.model('accident', accidentSchema, 'accident');
+
+module.exports = accidentModel;
---
+++ models/admin.model.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const uuid4 = require('uuidv4');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const schema = mongoose.Schema;
+
+
+const adminSchema = new schema(
+  {
+    uid: {
+      type: String,
+      default: uuid4(),
+      unique: true
+    },
+    first_name: {
+      type: String,
+      required: true,
+      unique: false
+    },
+    last_name: {
+      type: String,
+      required: true,
+      unique: false
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    is_admin: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    reset_password_token: {
+      type: String
+    },
+    reset_password_expires: {
+      type: Date
+    },
+    verify_token: {
+      type: String
+    },
+    is_verified: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt'
+    }
+  }
+);
+
+adminSchema.methods.isPasswordValid = (password, savedPassword) => {
+  const compare = bcrypt.compareSync(password, savedPassword);
+  return compare;
+};
+
+adminSchema.plugin(uniqueValidator);
+const userModel = mongoose.model('admin', adminSchema, 'admin');
+
+module.exports = userModel;
