a7dbd9bac4100e4cacc106e30774312068ef05a1
Dominic <otienodominic@gmail.com>
2020-09-17 12:06:09 +0300

    editted the package.json file

--- modules/twitter.js
+++ modules/twitter.js
@@ -1,66 +1,66 @@
-const {
-  CONSUMER_KEY,
-  CONSUMER_SECRET,
-  ACCESS_TOKEN,
-  ACCESS_TOKEN_SECRET,
-} = require("../config/key");
-const Twit = require("twit");
+// const {
+//   CONSUMER_KEY,
+//   CONSUMER_SECRET,
+//   ACCESS_TOKEN,
+//   ACCESS_TOKEN_SECRET,
+// } = require("../config/key");
+// const Twit = require("twit");
 
-const T = new Twit({
-  consumer_key: CONSUMER_KEY,
-  consumer_secret: CONSUMER_SECRET,
-  access_token: ACCESS_TOKEN,
-  access_token_secret: ACCESS_TOKEN_SECRET,
-});
+// const T = new Twit({
+//   consumer_key: CONSUMER_KEY,
+//   consumer_secret: CONSUMER_SECRET,
+//   access_token: ACCESS_TOKEN,
+//   access_token_secret: ACCESS_TOKEN_SECRET,
+// });
 
-class Tweet {
-    constructor(){
-        this.name = "Hello Tweet!"
-    }
+// class Tweet {
+//     constructor(){
+//         this.name = "Hello Tweet!"
+//     }
 
-    sayHi(){
-        return this.name
-    }
+//     sayHi(){
+//         return this.name
+//     }
 
-    // getFollowers(){
-    //     T.get('followers/list', { screen_name: 'DominicNgalo' }, (err, data, response) => {
-    //         if(!err){
-    //         console.log(data.users)
-    //         }
-    //       });
-    // }
+// getFollowers(){
+//     T.get('followers/list', { screen_name: 'DominicNgalo' }, (err, data, response) => {
+//         if(!err){
+//         console.log(data.users)
+//         }
+//       });
+// }
 
-    async follow(){
-      /*try {
+// async follow(){
+/*try {
         let data = await T.postFriendshipsCreate({screen_name: 'DominicNgalo'});
         console.log(data);
         return data;
       } catch (e) {console.log(e._response);
         throw e._response;
       }*/
-      return await T.postFriendshipsCreate({screen_name: 'DominicNgalo'})
-        .catch((e)=>{
-          throw e._response;
-        });
-      // await T.postFriendshipsCreate({screen_name: 'ochieng'})
-      //   .then((data)=>{
-      //     console.log(data);
-      //     return data;
-      //   })
-      //   .catch((error)=>{
-      //     console.log(error.message);
-      //     return error.message;
-      // });
+// return await T.postFriendshipsCreate({screen_name: 'DominicNgalo'})
+//   .catch((e)=>{
+//     throw e._response;
+//   });
+// await T.postFriendshipsCreate({screen_name: 'ochieng'})
+//   .then((data)=>{
+//     console.log(data);
+//     return data;
+//   })
+//   .catch((error)=>{
+//     console.log(error.message);
+//     return error.message;
+// });
 
-      // await T.post('friendships/create', {screen_name: 'DominicNgalo'}, (err, data, response) => {
-      //   if(err) {
-      //     console.log(err.message);
-      //     text = err.message;
-      //   }
-      // });
-      // console.log(err.message);
-      // return text;
-    }
-}
+// await T.post('friendships/create', {screen_name: 'DominicNgalo'}, (err, data, response) => {
+//   if(err) {
+//     console.log(err.message);
+//     text = err.message;
+//   }
+// });
+// console.log(err.message);
+// return text;
+//     }
+// }
 
-module.exports = Tweet;
+// module.exports = Tweet;
