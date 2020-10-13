475ead2359166837eac4a6f5ad59b1b11071633f
Dominic <otienodominic@gmail.com>
2020-10-13 10:49:02 +0300

    environment matters

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,7 @@
 module.exports={
-  MONGOURI:process.env.MOGOURI,
+  MONGOURI:process.env.MONGODB_URI,
   JWT_SECRET:process.env.JWT_SEC,
   SENDGRID_API:process.env.SENDGRID_API,
-  EMAIL:process.env.EMAIL
+  EMAIL:process.env.EMAIL, 
+  PORT: process.env.PORT
 }
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 5000;
-const { MONGOURI } = require("./config/keys");
+// const PORT = process.env.PORT || 5000;
+const { MONGOURI, PORT } = require("./config/keys");
 
-mongoose.connect(process.env.MONGODB_URI || MONGOURI, {
+mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
